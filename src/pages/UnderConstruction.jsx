cc3066e496c01d3b0c52d757e26b41cc36467561
Lillian Layne <144570372+lillianlayne@users.noreply.github.com>
2024-05-19 17:26:58 -0400

    padding

--- src/pages/UnderConstruction.jsx
+++ src/pages/UnderConstruction.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const UnderConstruction = () => {
   return (
-    <div className='w-screen h-screen font-serif flex justify-center items-center'>
+    <div className='w-screen h-screen p-6 font-serif flex justify-center items-center'>
       <h1 className="text-3xl text-stone-800">
         This section is currently <em>in the works...</em> 
       </h1>
